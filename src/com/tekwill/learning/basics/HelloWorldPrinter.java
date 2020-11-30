5c15c550a4e9b692117b1423ff2c6ed5ae5755b5
Marius2772 <cebotarimarius27@gmail.com>
2020-11-30 20:01:52 +0200

    Hello people

--- src/com/tekwill/learning/basics/HelloWorldPrinter.java
+++ src/com/tekwill/learning/basics/HelloWorldPrinter.java
@@ -1,8 +1,8 @@
 package com.tekwill.learning.basics;
 
 public class HelloWorldPrinter {
-    public static void main(String[]cargs){
-        System.out.println("Hello, World");
+    public static void main(String[] args) {
+        System.out.println("Hello, people");
 
     }
 }
