2f75268e0b43e187a9484ec9e18dca5e849f4e8d
Marius2772 <cebotarimarius27@gmail.com>
2020-12-01 11:16:03 +0200

    FirstOne

---
+++ src/com/tekwill/learning/basics/FirstOne.java
@@ -0,0 +1,13 @@
+package com.tekwill.learning.basics;
+
+public class FirstOne {
+    public static void main(String[] args) {
+        System.out.println("A, \"quoted\", String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of \"escape sequances\"");
+        System.out.println("");
+        System.out.println("Also, \"\" represents an empty Strig.");
+        System.out.println("Dont't forget: use \\ \" instead of \" !");
+        System.out.println("'' is not the same as \" ");
+    }
+}
