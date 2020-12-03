d2e87798b7a315198c7ed1a1447130b2b8e4695f
Marius2772 <cebotarimarius27@gmail.com>
2020-12-03 10:31:39 +0200

    FightSong

---
+++ src/com/tekwill/learning/basics/FightSong.java
@@ -0,0 +1,26 @@
+package com.tekwill.learning.basics;
+
+public class FightSong {
+    public static void main(String[] args) {
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+        System.out.println("");
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+        System.out.println("");
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+        System.out.println("You're the best,");
+        System.out.println("in the West.");
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+        System.out.println("");
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+
+    }
+}
