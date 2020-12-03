9075872001b4b35192638b5d7157591b9d234522
Marius2772 <cebotarimarius27@gmail.com>
2020-12-03 10:40:49 +0200

    Lanterns

---
+++ src/com/tekwill/learning/basics/Lanterns.java
@@ -0,0 +1,27 @@
+package com.tekwill.learning.basics;
+
+public class Lanterns {
+    public static void main(String[] args) {
+        System.out.println("    *****");
+        System.out.println("  *********");
+        System.out.println("*************");
+        System.out.println("");
+        System.out.println("    *****");
+        System.out.println("  *********");
+        System.out.println("*************");
+        System.out.println("* | | | | | *");
+        System.out.println("*************");
+        System.out.println("    *****");
+        System.out.println("  *********");
+        System.out.println("*************");
+        System.out.println("");
+        System.out.println("    *****");
+        System.out.println("  *********");
+        System.out.println("*************");
+        System.out.println("    *****");
+        System.out.println("* | | | | | *");
+        System.out.println("* | | | | | *");
+        System.out.println("    *****");
+        System.out.println("    *****");
+    }
+}
