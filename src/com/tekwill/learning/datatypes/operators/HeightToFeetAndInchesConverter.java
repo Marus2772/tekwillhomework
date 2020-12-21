8bd5e60cac11c08764dee168dd40c55ab359f7bc
Marius2772 <cebotarimarius27@gmail.com>
2020-12-21 16:13:39 +0200

    heightmodifier

---
+++ src/com/tekwill/learning/datatypes/operators/HeightToFeetAndInchesConverter.java
@@ -0,0 +1,17 @@
+package com.tekwill.learning.datatypes.operators;
+
+import java.util.Scanner;
+
+public class HeightToFeetAndInchesConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your height in inches: (Should be in range 60 - 85 inches)");
+        int height = scanner.nextInt();
+        int heightInFeet = height / 12;
+        int inches = height % 12;
+
+        System.out.println("Your height is " + heightInFeet + " feet, " + inches + " inches");
+
+
+    }
+}
