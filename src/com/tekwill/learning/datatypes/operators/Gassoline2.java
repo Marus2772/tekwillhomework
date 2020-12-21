2d01153dd0dab5d5dc091ee2a10f8c5832974b4b
Marius2772 <cebotarimarius27@gmail.com>
2020-12-21 16:34:46 +0200

    Gassoline2

---
+++ src/com/tekwill/learning/datatypes/operators/Gassoline2.java
@@ -0,0 +1,19 @@
+package com.tekwill.learning.datatypes.operators;
+
+import java.util.Scanner;
+
+public class Gassoline2 {
+    public static final float KM_TO_MILES_CONVERTER = 0.621371F;
+    public static final float GALON_TO_LITERS_CONVERTER = 3.7854F;
+
+    public static float EU_to_US_ConsumptionConverter(float consumptionInEU_Format) {
+        return 1 / ((consumptionInEU_Format / GALON_TO_LITERS_CONVERTER) / (KM_TO_MILES_CONVERTER * 100));
+    }
+
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Please enter automobile gasoline consumption in liters/100km:");
+        float consumption = scaner.nextFloat();
+        System.out.printf("The consumption in US format is: %.2f MPG", EU_to_US_ConsumptionConverter(consumption));
+    }
+}
