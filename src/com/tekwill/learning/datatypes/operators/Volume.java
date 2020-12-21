5186dded48cd682aa767b76010580a383f4d1211
Marius2772 <cebotarimarius27@gmail.com>
2020-12-21 16:37:28 +0200

    Volume

--- src/com/tekwill/learning/datatypes/operators/Volume.java
+++ src/com/tekwill/learning/datatypes/operators/Volume.java
@@ -3,13 +3,13 @@ package com.tekwill.learning.datatypes.operators;
 public class Volume {
     public static final double PI = Math.PI;
 
-    public static double sphereVolume(double r) {
-        double sphereVolume = 4 / 3D * PI * Math.pow(r, 3);
-        return sphereVolume;
+    public static double TheVolume(double r) {
+        double TheVolume = 4 / 3D * PI * Math.pow(r, 3);
+        return TheVolume;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(sphereVolume(10));
+        System.out.println(TheVolume(10));
     }
 }
