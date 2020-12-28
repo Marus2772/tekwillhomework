73866147209bb84695019700785871ae7279ce3e
Marius2772 <cebotarimarius27@gmail.com>
2020-12-28 14:25:06 +0200

    DollarExchange

---
+++ src/com/tekwill/learning/datatypes/operators/DollarExchange.java
@@ -0,0 +1,15 @@
+package com.tekwill.learning.datatypes.operators;
+
+import java.util.Scanner;
+
+public class DollarExchange {
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Dollar amount:");
+        int amount = scanner.nextInt();
+        System.out.println("$20 Bills: " + amount / 20 +
+                "\n$10 Bills: " + (amount % 20) / 10 +
+                "\n $5 Bills: " + ((amount % 20) % 10) / 5 +
+                "\n $1 Bills: " + ((amount % 20) % 10) % 5);
+    }
+}
