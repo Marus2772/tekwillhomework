73a2354035562aab3f60a3cd64a530fb7fa3c7ad
Marius2772 <cebotarimarius27@gmail.com>
2021-01-03 14:36:40 +0200

    SpeedOfSound

--- src/com/tekwill/learning/loops/SpeedOfSound.java
+++ src/com/tekwill/learning/loops/SpeedOfSound.java
@@ -1,4 +1,27 @@
 package com.tekwill.learning.loops;
 
+import java.util.Scanner;
+
 public class SpeedOfSound {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter \"air\", \"water\" or \"steel\" and the distance in feet");
+        String env = scanner.next();
+        double distance = scanner.nextDouble();
+
+        switch (env) {
+            case "air":
+                System.out.printf("It takes %.2f seconds for sound to travel %.2f feet through the air", distance / 1100, distance);
+                break;
+            case "water":
+                System.out.printf("It takes %.2f seconds for sound to travel %.2f feet through the water", distance / 4900, distance);
+                break;
+            case "steel":
+                System.out.printf("It takes %.2f seconds for sound to travel %.2f feet through the steel", distance / 16400, distance);
+                break;
+            default:
+                System.out.println("Error!!! Wrong input");
+        }
+
+    }
 }
