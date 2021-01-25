6e51231fc6aa7671ac6a4e6ed759dd4819e444a4
Marius2772 <cebotarimarius27@gmail.com>
2021-01-25 14:10:26 +0200

    DisplayMatrix

---
+++ src/com/tekwill/learning/strings/DisplayMatrix.java
@@ -0,0 +1,23 @@
+package com.tekwill.learning.strings;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DisplayMatrix {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sign = random.nextInt(2);
+                if (sign == 1) {
+                    System.out.print("\u2B1B ");
+                } else
+                    System.out.print("\u2B1A ");
+            }
+            System.out.println();
+        }
+    }
+}
